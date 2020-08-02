15a0106c3227e6b7208389ea0ccc7bb73bada5f1
BeijiYang <langyuyang@outlook.com>
2020-08-02 22:13:14 +0200

    parse the response body using state machines

--- client/index.js
+++ client/index.js
@@ -108,15 +108,24 @@ class ResponseParser {
     this.headerValue = '';
     this.bodyParser = null;
   }
-  get isFinished() { }
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
 
-  get response() { }
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
 
   receive(string) {
     for (const char of string) {
       this.receiveChar(char);
     }
-    console.log(this.headers)
   }
   // "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: Sun, 02 Aug 2020 17:05:31 GMT\r\nConnection: keep-alive\r\nTransfer-Encoding: chunked\r\n\r\nd\r\n Hello World\n\r\n0\r\n\r\n"  // a state machine
   // a state machine
@@ -142,6 +151,11 @@ class ResponseParser {
         } else if (char === '\r') {
           // no key-value pairs at all or the end of k-v paris (no more)
           this.current = this.WAITING_HEADER_BLOCK_END;
+          // Transfer-Encoding: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Transfer-Encoding
+          // "chunked" is the default value in node. we use it as the example.
+          if (this.headers['Transfer-Encoding'] === 'chunked') {
+            this.bodyParser = new ChunkedBodyParser();
+          }
         } else {
           this.headerName += char;
         }
@@ -152,6 +166,7 @@ class ResponseParser {
           this.current = this.WAITING_HEADER_VALUE;
         }
         break;
+
       case this.WAITING_HEADER_VALUE:
         if (char === '\r') {
           this.current = this.WAITING_HEADER_LINE_END;
@@ -179,8 +194,76 @@ class ResponseParser {
         break;
 
       case this.WAITING_BODY:
-        // console.log((char));
-        console.log(JSON.stringify(char));
+        // console.log(JSON.stringify(char));
+        this.bodyParser.receiveChar(char);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+// a state machine
+class ChunkedBodyParser {
+  constructor() {
+    // d\r\n Hello World\n\r\n0\r\n\r\n
+    // d...\r\n.... Hello World\n....\r\n
+    // 0....\r\n....\r\n
+    this.WAITING_LENGTH = 0;
+    this.WAITING_LENGTH_LINE_END = 1;
+
+    this.READING_CHUNK = 2;
+    this.WAITING_BLANK_LINE = 3;
+    this.WAITING_BLANK_LINE_END = 4;
+
+    this.current = this.WAITING_LENGTH;
+
+    this.content = [];
+    this.length = 0;
+    this.isFinished = false;
+  }
+
+  receiveChar(char) {
+    switch (this.current) {
+      case this.WAITING_LENGTH:
+        if (char === '\r') {
+          this.current = this.WAITING_LENGTH_LINE_END;
+          if (this.length === 0) {
+            this.isFinished = true;
+          }
+        } else {
+          // 十六进制数，一位一位往过读取。如，2AF5，先读 2，再进位，读A，依次类推...
+          this.length *= 16;
+          this.length += parseInt(char, 16);
+        }
+        break;
+
+      case this.WAITING_LENGTH_LINE_END:
+        if (char === '\n' && !this.isFinished) {
+          this.current = this.READING_CHUNK;
+        }
+        break;
+
+      case this.READING_CHUNK:
+        this.content.push(char);
+        this.length -= 1;
+        // this.length--;
+        if (this.length === 0) {
+          this.current = this.WAITING_BLANK_LINE;
+        }
+        break;
+
+      case this.WAITING_BLANK_LINE:
+        if (char === '\r') {
+          this.current = this.WAITING_BLANK_LINE_END;
+        }
+        break;
+
+      case this.WAITING_BLANK_LINE_END:
+        if (char === '\n') {
+          this.current = this.WAITING_LENGTH;
+        }
         break;
 
       default:
@@ -203,8 +286,8 @@ void async function () {
     }
   });
 
-  const reponse = await request.send();
-  // console.log(response);
+  const response = await request.send();
+  console.log(response);
   // the format of response: status line \r 空行 \r headers \r body 
   // 其中 body：node 默认格式 chunked body: 16进制数字表长度 内容 直到最后是0
 }()
