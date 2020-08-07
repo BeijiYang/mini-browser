a917071adc9a3c3dee2f5bbd56d0ef94a7546a90
BeijiYang <langyuyang@outlook.com>
2020-08-07 18:17:01 +0200

    make the server send back a html

--- server/server.js
+++ server/server.js
@@ -13,7 +13,28 @@ http.createServer((request, response) => {
     .on('end', () => {
       body = body.join('');
       response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end(' Hello World\n');
+      // response.end(' Hello World\n');
+      response.end(
+        `<html maaa=a >
+    <head>
+          <style>
+    body div #myid{
+      width:100px;
+      background-color: #ff5000;
+    }
+    body div img{
+      width:30px;
+      background-color: #ff1111;
+    }
+      </style>
+    </head>
+    <body>
+      <div>
+          <img id="myid"/>
+          <img />
+      </div>
+    </body>
+    </html>`);
     });
 }).listen(8080);
 
